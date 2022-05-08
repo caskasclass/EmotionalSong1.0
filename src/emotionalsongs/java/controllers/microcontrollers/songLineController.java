5ee7f1d3134f8cf3b6d9f1055c1eff8d55175db6
caskasclass <caskasclass@gmail.com>
2022-05-08 17:47:03 +0200

    ho passato circa 8 ore a fare un cazzo di menu a tendina, dio ... non si dice

--- src/emotionalsongs/java/controllers/microcontrollers/songLineController.java
+++ src/emotionalsongs/java/controllers/microcontrollers/songLineController.java
@@ -5,11 +5,21 @@ import java.util.ResourceBundle;
 
 import emotionalsongs.java.util.Canzone;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
 
 public class songLineController implements Initializable {
+    @FXML
+    private MenuButton buttonContext;
+    @FXML
+    private GridPane rowLine;
     @FXML
     private Label lb_SongNumber;
     @FXML
@@ -28,7 +38,6 @@ public class songLineController implements Initializable {
     private Canzone canzone;
     private Integer indx;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Platform.runLater(() -> {
@@ -39,14 +48,30 @@ public class songLineController implements Initializable {
             lb_SongGenere.setText(canzone.getGenere());
             lb_SongAnno.setText(canzone.getAnno().toString());
             lb_SongDurata.setText(canzone.getDurata().toString());
-        });       
+        });
     }
-    
-    public void setCanzone(Canzone c,Integer index)
-    {
-        canzone = c ;
+
+    public void setCanzone(Canzone c, Integer index) {
+        canzone = c;
         indx = index;
     }
-    
 
+    public void addToPlaylistEvent(ActionEvent e) {
+        System.out.println("Hai cliccato aggiungi alla playlist");
+    }
+    
+    public void selectRow(MouseEvent e){
+        VBox p = (VBox) rowLine.getParent();
+        p.getChildren().forEach(node -> {
+            node.getStyleClass().remove("nothover");
+        });
+        this.rowLine.getStyleClass().add("nothover");
+        if(this.rowLine.getStyleClass().contains("nothover")){
+
+            buttonContext.setVisible(true);
+        }
+        
+        
+    }
+   
 }
