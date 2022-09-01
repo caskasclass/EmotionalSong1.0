065720f4e96763a789e7b0f9ea95e68b14b09c14
caskasclass <caskasclass@gmail.com>
2022-09-01 16:14:22 +0200

    turututtto

---
+++ SODetect.sh
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+echo $OSTYPE;
+
+if [[ "$OSTYPE" == "darwin"* ]];
+ then
+       java -jar --enable-preview --module-path lib/ --add-modules=javafx.controls,javafx.media,javafx.fxml,javafx.graphics EmotionalSong1.0.jar
+elif [[ "$OSTYPE" == "win"* ]];
+then 
+    echo "ok";
+fi
